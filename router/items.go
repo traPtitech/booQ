74a7d8aa0f251164a3bbc0542d142be549cb35c2
ryoha000 <the7uya@gmail.com>
2019-08-23 16:36:44 +0900

    PostItemsのテストを追加しました.

--- router/items.go
+++ router/items.go
@@ -10,9 +10,14 @@ import (
 
 // PostItems POST /items
 func PostItems(c echo.Context) error {
+	user := c.Get("user").(model.User)
 	item := model.Item{}
 	if err := c.Bind(&item); err != nil {
 		return err
+	}
+	// item.Type=0⇒trap所有、1⇒支援課、2⇒個人
+	if item.Type !=2 && !user.Admin {
+		return c.NoContent(http.StatusForbidden)
 	}
 	res, err := model.CreateItem(item)
 	if err != nil {
---
+++ router/items_test.go
@@ -0,0 +1,87 @@
+package router
+
+import (
+	"bytes"
+	"encoding/json"
+	// "io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/labstack/echo"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/traPtitech/booQ/model"
+)
+
+func TestPostItems(t *testing.T) {
+	t.Parallel()
+
+	testBodyTrap := model.Item{
+		Name:        "testTrapItem",
+		Type:0,
+		Code:1920093013000,
+		Description:"これは備品のテストです",
+		ImgURL:"http://testTrap.jpg",
+	}
+
+	testBodyKojin := model.Item{
+		Name:        "testKojinItem",
+		Type:2,
+		Code:9784049123944,
+		Description:"これは個人所有物のテストです",
+		ImgURL:"http://testKojin.jpg",
+	}
+
+	t.Run("admin user", func(t *testing.T) {
+		assert := assert.New(t)
+		e := echoSetupWithAdminUser()
+
+		reqBody, _ := json.Marshal(testBodyTrap)
+		req := httptest.NewRequest(echo.POST, "/api/items", bytes.NewReader(reqBody))
+		req.Header.Set("Content-Type", "application/json")
+		rec := httptest.NewRecorder()
+		e.ServeHTTP(rec, req)
+
+		assert.Equal(http.StatusOK, rec.Code)
+
+		item := model.Item{}
+		_ = json.NewDecoder(rec.Body).Decode(&item)
+
+		assert.Equal(testBodyTrap.Name, item.Name)
+		assert.Equal(testBodyTrap.Type, item.Type)
+		assert.Equal(testBodyTrap.Code, item.Code)
+		assert.Equal(testBodyTrap.Description, item.Description)
+		assert.Equal(testBodyTrap.ImgURL, item.ImgURL)
+	})
+
+	t.Run("not admin user", func(t *testing.T) {
+		assert := assert.New(t)
+		e := echoSetupWithUser()
+
+		reqBody, _ := json.Marshal(testBodyTrap)
+		req := httptest.NewRequest(echo.POST, "/api/items", bytes.NewReader(reqBody))
+		req.Header.Set("Content-Type", "application/json")
+		rec := httptest.NewRecorder()
+		e.ServeHTTP(rec, req)
+
+		assert.Equal(http.StatusForbidden, rec.Code)
+
+		reqBody, _ = json.Marshal(testBodyKojin)
+		req = httptest.NewRequest(echo.POST, "/api/items", bytes.NewReader(reqBody))
+		req.Header.Set("Content-Type", "application/json")
+		rec = httptest.NewRecorder()
+		e.ServeHTTP(rec, req)
+
+		assert.Equal(http.StatusOK, rec.Code)
+
+		item := model.Item{}
+		_ = json.NewDecoder(rec.Body).Decode(&item)
+
+		assert.Equal(testBodyKojin.Name, item.Name)
+		assert.Equal(testBodyKojin.Type, item.Type)
+		assert.Equal(testBodyKojin.Code, item.Code)
+		assert.Equal(testBodyKojin.Description, item.Description)
+		assert.Equal(testBodyKojin.ImgURL, item.ImgURL)
+	})
+}
