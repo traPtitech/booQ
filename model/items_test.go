1a96fa035dbe03795a0c6af5f227b8eb1b3b8c8c
ryoha000 <48485650+ryoha000@users.noreply.github.com>
2019-09-05 09:44:51 +0900

    ノートpcに移行用

--- model/items_test.go
+++ model/items_test.go
@@ -32,3 +32,26 @@ func TestCreateItem(t *testing.T) {
 		assert.NotEmpty(item)
 	})
 }
+
+func TestRegisterOwner(t *testing.T) {
+	t.Parallel()
+
+	// failする要素が分らなかったのでfailを書いてません
+
+	t.Run("success", func(t *testing.T) {
+		t.Parallel()
+		assert := assert.New(t)
+
+		user, _ := CreateUser(User{Name: "registerTestOwner"})
+		var owner Owner
+		owner.Owner = user
+		owner.Rentalable = true
+		item, _ := CreateItem(Item{Name: "registerTestItem"})
+		item, err := RegisterOwner(owner, item)
+
+		assert.NoError(err)
+		assert.NotEmpty(item)
+		// fmt.Println(item)
+		// assert.Equal(item.Owners[1].Owner.Name, user.Name)
+	})
+}
