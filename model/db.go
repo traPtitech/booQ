d3bac585db06bf05d8f3a59df6a582f0cbfb5c80
T Nagano <nagatea@users.noreply.github.com>
2019-09-24 23:59:37 +0900

    Remove tab space

--- model/db.go
+++ model/db.go
@@ -48,7 +48,7 @@ func Migrate() error {
 	if err := db.AutoMigrate(allTables...).Error; err != nil {
 		return err
 	}
-	
+
 	traP, err := GetUser(User{Name: "traP"})
 	if traP.Name == "" {
 		user := User{
