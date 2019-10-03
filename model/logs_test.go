776f74e6fbbb1bd4b0ed8e0af215e45e8048961b
ryoha000 <48485650+ryoha000@users.noreply.github.com>
2019-10-03 21:18:54 +0900

    消す個所を間違えました

--- model/logs_test.go
+++ model/logs_test.go
@@ -1,1 +1,41 @@
 package model
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLogTableName(t *testing.T) {
+	t.Parallel()
+	assert.Equal(t, "logs", (&Log{}).TableName())
+}
+
+func TestCreateLog(t *testing.T) {
+	t.Parallel()
+
+	t.Run("failures", func(t *testing.T) {
+		assert := assert.New(t)
+
+		log, err := CreateLog(Log{})
+		assert.Error(err)
+		assert.Empty(log)
+
+		log, err = CreateLog(Log{ItemID: 66, OwnerID: 66})
+		assert.Error(err)
+		assert.Empty(log)
+	})
+
+	t.Run("success", func(t *testing.T) {
+		assert := assert.New(t)
+
+		owner, _ := GetUserByName("traP")
+		item, _ := CreateItem(Item{Name: "testItemForCreateLog"})
+
+		log, err := CreateLog(Log{ItemID: int(item.ID), OwnerID: int(owner.ID)})
+		assert.NoError(err)
+		assert.NotEmpty(log)
+		assert.Equal(int(owner.ID), log.OwnerID)
+		assert.Equal(int(item.ID), log.ItemID)
+	})
+}
