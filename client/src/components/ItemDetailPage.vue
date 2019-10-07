0c37f49a7b1c5a16a0ff9631f95e52ce979c3add
nagatech <cheesecakebot@yahoo.co.jp>
2019-10-08 00:23:33 +0900

    Add log message

--- client/src/components/ItemDetailPage.vue
+++ client/src/components/ItemDetailPage.vue
@@ -11,10 +11,12 @@
               />
             </div>
             <div>
-              <!-- TODO: 返す場合のボタンの処理を考える -->
+              <!-- TODO: ボタンの処理を考える -->
               <v-btn block color="primary">借りる</v-btn>
+              <v-btn block color="warning">返す</v-btn>
             </div>
-            <div>
+            <!-- TODO: お気に入り機能まわりを実装したら復活させてね -->
+            <!-- <div>
               <v-btn v-if="isLiked" block @click="removeLike">
                 <v-icon left color="indigo">mdi-thumb-up</v-icon>
                 いいね {{ likeCount }}
@@ -27,10 +29,13 @@
             <div>
               <v-layout row wrap class="d-inline-flex">
                 <v-flex v-for="like in data.likes" :key="like.id" >
-                  <Icon :user="like" />
+                  <Icon
+                    :user="like"
+                    :size="25"
+                  />
                 </v-flex>
               </v-layout>
-            </div>
+            </div> -->
           </div>
         </v-col>
       </v-row>
@@ -47,9 +52,13 @@
             <v-icon dark>mdi-plus</v-icon>
           </v-btn>
         </h2>
+        <!-- FIXME: 他のタスクに手をつけたかったので表示が適当です -->
         <div v-for="owner in data.owners" :key="owner.id">
-          <p v-if="checkRentalable(owner.user.id)">{{owner.user.name}}  {{checkRentalable(owner.user.id)}}</p>
-          <p v-else v-on:click="rental">{{owner.user.name}}  貸し出し可</p>
+          <Icon
+            :user="owner.user"
+            :size="25"
+          />
+          {{ owner.user.name }} {{ checkRentalable(owner) }}
         </div>
       </div>
       <div class="content">
@@ -71,7 +80,13 @@
       <div class="content">
         <div>
           <h2>ログ</h2>
-          工事中
+          <div v-for="log in reverseLogs" :key="log.id">
+            <Icon
+              :user="log.user"
+              :size="25"
+            />
+            {{ createLogMessage(log) }} - {{ log.created_at }}
+          </div>
         </div>
       </div>
     </div>
@@ -98,11 +113,22 @@ export default {
           {
             user: {
               id: 1,
+              name: 'traP',
+              displayName: 'traP',
+              admin: true
+            },
+            rentalable: true,
+            count: 1
+          },
+          {
+            user: {
+              id: 2,
               name: 'nagatech',
               displayName: 'ながてち',
               admin: true
             },
-            rentalable: true
+            rentalable: true,
+            count: 1
           }
         ],
         description: '高校1年の夏、帆高（ほだか）は離島から家出し、東京にやってきた。連日降り続ける雨の中、雑踏ひしめく都会の片隅で、帆高は不思議な能力を持つ少女・陽菜（ひな）に出会う。「ねぇ、今から晴れるよ」。それは祈るだけで、空を晴れに出来る力だった――。天候の調和が狂っていく時代に、運命に翻弄される少年と少女が自らの生き方を「選択」する物語。長編アニメーション映画『天気の子』の、新海誠監督自身が執筆した原作小説。',
@@ -126,9 +152,9 @@ export default {
             id: 1,
             item_id: 1,
             user: {
-              id: 1,
-              name: 'nagatech',
-              displayName: 'ながてち',
+              id: 2,
+              name: 'ryoha',
+              displayName: 'りょは',
               admin: true
             },
             owner: {
@@ -139,6 +165,51 @@ export default {
             },
             type: 0,
             purpose: '読みたかったから。',
+            count: 1,
+            due_date: '2019/07/30 23:30:00',
+            created_at: '2019/07/28 22:00:00',
+            updated_at: '2019/07/28 22:00:00'
+          },
+          {
+            id: 2,
+            item_id: 1,
+            user: {
+              id: 2,
+              name: 'ryoha',
+              displayName: 'りょは',
+              admin: true
+            },
+            owner: {
+              id: 1,
+              name: 'nagatech',
+              displayName: 'ながてち',
+              admin: true
+            },
+            type: 1,
+            purpose: '',
+            count: 1,
+            due_date: '2019/07/30 23:30:00',
+            created_at: '2019/07/28 23:00:00',
+            updated_at: '2019/07/28 22:00:00'
+          },
+          {
+            id: 3,
+            item_id: 1,
+            user: {
+              id: 1,
+              name: 'nagatech',
+              displayName: 'ながてち',
+              admin: true
+            },
+            owner: {
+              id: 3,
+              name: 'traP',
+              displayName: 'traP',
+              admin: true
+            },
+            type: 0,
+            purpose: '読みたかったから。',
+            count: 1,
             due_date: '2019/07/30 23:30:00',
             created_at: '2019/07/28 22:00:00',
             updated_at: '2019/07/28 22:00:00'
@@ -194,6 +265,9 @@ export default {
     window.removeEventListener('resize', this.conputeWidth)
   },
   computed: {
+    reverseLogs () {
+      return this.data.logs.slice().reverse()
+    },
     likeCount () {
       return this.data.likes.length
     },
@@ -207,17 +281,34 @@ export default {
   methods: {
     conputeWidth () {
       if (window.innerWidth > 961) {
-        this.contentWidth = window.innerWidth - 600
+        this.contentWidth = window.innerWidth - 545
       } else if (window.innerWidth > 601) {
-        this.contentWidth = window.innerWidth - 340
+        this.contentWidth = window.innerWidth - 290
       } else {
-        this.contentWidth = window.innerWidth
+        this.contentWidth = window.innerWidth - 30
       }
     },
-    checkRentalable (ownerID) {
-      // いい感じにしてください。同じownerが複数いるときのロジックがわかりませんでした
-      // 貸し出し可ならfalseを返し不可なら'ryohaが借りてます'みたいなのを返すと思ってます
-      return false
+    checkRentalable (owner) {
+      // FIXME: ロジックがやばい
+      if (owner.rentalable) {
+        return '貸し出し可能'
+      } else {
+        return '貸し出しできません'
+      }
+    },
+    createLogMessage (log) {
+      const userName = log.user.name
+      const ownerName = log.owner.name
+      const ownerWord = ownerName === 'traP' ? '' : `${ownerName}さんの`
+      const logComment = log.type === 0 ? '借りました' : '返しました'
+      return `${userName}さんが${ownerWord}物品を${logComment}`
+    },
+    checkLogType (log) {
+      if (log.type === 0) {
+        return '借りました'
+      } else {
+        return '返しました'
+      }
     },
     like () {
       this.data.likes.push(this.$store.state.me)
