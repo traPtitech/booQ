975cff6030fda05ef7a3b745053cd3a812a2ab71
ryoha000 <48485650+ryoha000@users.noreply.github.com>
2019-10-07 17:43:13 +0900

    微修正

--- client/src/components/shared/ItemList.vue
+++ client/src/components/shared/ItemList.vue
@@ -17,42 +17,43 @@
             :key="item.id"
             style="height: 100px;"
           >
-            <img
-              :src="item.img_url"
-              class="item-list-image"
-              :to="`/items/${item.id}`"
-            />
-            <v-list-item-content style="padding-left: 15px;" :to="`/items/${item.id}`">
-              <v-list-item-title class="headline mb-1">{{ item.name }}</v-list-item-title>
-              <v-list-item-subtitle>{{ item.owners.map(i => i.user.name).join(', ') }}</v-list-item-subtitle>
-            </v-list-item-content>
+            <v-list-item :to="`/items/${item.id}`">
+              <img
+                :src="item.img_url"
+                class="item-list-image"
+              />
+              <v-list-item-content style="padding-left: 15px;" :to="`/items/${item.id}`">
+                <v-list-item-title class="headline mb-1">{{ item.name }}</v-list-item-title>
+                <v-list-item-subtitle>{{ item.owners.map(i => i.user.name).join(', ') }}</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item-action class="item-list-icons">
+              <v-btn icon v-if="item.type == 0" @click.stop="click2Cart()" absolute>
+                <v-icon>mdi-cart-arrow-down</v-icon>
+              </v-btn>
+              <div class="text-center">
+                <v-dialog v-model="isOpen2Cart" max-width="290">
+                  <v-card width="290">
+                    <v-card-title class="headline grey lighten-2" primary-title>
+                      個数を選択
+                    </v-card-title>
+                    <v-card-actions>
+                      <v-slider :max="getBihinLatestCount(item.id)" v-model="item.rentalCount" thumb-label="always" />
+                    </v-card-actions>
+                    <v-divider></v-divider>
+                    <v-card-actions>
+                      <v-btn @click="putItem2Cart(item)" primary>
+                        Put Cart
+                      </v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>
+              </div>
+            </v-list-item-action>
             <v-list-item-action>
-                <v-btn icon v-if="item.type == 0" @click.stop="click2Cart()" absolute>
-                  <v-icon>mdi-cart-arrow-down</v-icon>
-                </v-btn>
-                <div class="text-center">
-                  <v-dialog v-model="isOpen2Cart" max-width="290">
-                    <v-card width="280">
-                      <v-card-title class="headline grey lighten-2" primary-title>
-                        個数を選択
-                      </v-card-title>
-                      <v-card-actions>
-                        <v-slider :max="getBihinLatestCount(item.id)" v-model="item.rentalCount" thumb-label="always" />
-                      </v-card-actions>
-                      <v-divider></v-divider>
-                      <v-card-actions>
-                        <v-btn @click="putItem2Cart(item)" primary>
-                          Put Cart
-                        </v-btn>
-                      </v-card-actions>
-                    </v-card>
-                  </v-dialog>
-                </div>
-              </v-list-item-action>
-            <v-list-item-icon disabled v-if="item.like_counts">
               <v-icon>thumb_up_alt</v-icon>
               {{ item.like_counts }}
-            </v-list-item-icon>
+            </v-list-item-action>
           </v-list-item>
         </v-list-item-group>
       </v-list>
@@ -97,5 +98,9 @@ export default {
 .item-list-image {
   max-height: 100px;
   max-width: 120px;
+}
+.item-list-icons {
+  max-height: 80px;
+  max-width: 80px;
 }
 </style>
