ef796da57b7c6dcde741047037971bf7e8840d1f
ryoha000 <48485650+ryoha000@users.noreply.github.com>
2019-10-08 18:57:08 +0900

    元に戻しました

--- model/logs.go
+++ model/logs.go
@@ -77,7 +77,7 @@ func GetLatestLogs(logs []Log) ([]Log, error) {
 	for _, log := range logs {
 		nowLatestLog, exist := logMap[log.OwnerID]
 		if exist {
-			if nowLatestLog.CreatedAt.Before(log.CreatedAt) {
+			if !nowLatestLog.CreatedAt.After(log.CreatedAt) {
 				logMap[log.OwnerID] = log
 			}
 		} else {
