17095e65cd838cee22f88888c569c927564da2df
WistreHosshii <sao_heath6147.wistre@icloud.com>
2019-10-19 14:38:09 +0900

    alertようのコンポーネントの追加

---
+++ client/src/components/AleartDialog.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <v-dialog v-model="alert.isAlert" persistent>
+      <v-alert :type="alert.alertType" v-model="alert.isAlert" dismissible  >
+        <h3>{{this.alert.title}}</h3>
+        {{this.alert.message}}
+      </v-alert>
+    </v-dialog>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    alert: {
+      type: Object,
+      default: () => ({
+        isAlert: false,
+        alertType: 'error',
+        title: 'Error',
+        message: 'error'
+      })
+    }
+  }
+}
+</script>
