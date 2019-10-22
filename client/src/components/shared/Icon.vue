5b78da2afe190706d6c80151e355f4dd60808873
series2 <series2.others@gmail.com>
2019-10-22 16:04:05 +0900

    icon to userpage

--- client/src/components/shared/Icon.vue
+++ client/src/components/shared/Icon.vue
@@ -1,7 +1,13 @@
 <template>
+<v-btn
+          dark
+          icon
+          :to="`/users/${user.name}`"
+        >
   <v-avatar :size="size">
     <v-img :src="'https://q.trap.jp/api/1.0/public/icon/' + user.name" width="100%" alt=""/>
   </v-avatar>
+</v-btn>
 </template>
 
 <script>
