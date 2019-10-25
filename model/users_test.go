183c3084258af2e4e6e57161178cf031241ca393
nagatech <cheesecakebot@yahoo.co.jp>
2019-10-25 15:30:53 +0900

    Fix UpdateUser bug

--- model/users_test.go
+++ model/users_test.go
@@ -55,10 +55,7 @@ func TestCreateUser(t *testing.T) {
 }
 
 func TestUpdateUser(t *testing.T) {
-	t.Parallel()
-
 	t.Run("failures", func(t *testing.T) {
-		t.Parallel()
 		assert := assert.New(t)
 
 		user, err := UpdateUser(User{DisplayName: "test3display"})
@@ -69,19 +66,20 @@ func TestUpdateUser(t *testing.T) {
 	t.Run("success", func(t *testing.T) {
 		assert := assert.New(t)
 
-		user1, err := CreateUser(User{Name: "test3", DisplayName: "displaytest3", Admin: true})
+		user1, err := CreateUser(User{Name: "test3", DisplayName: "test3before", Admin: true})
 		assert.NoError(err)
 		assert.NotEmpty(user1)
 
-		user, err := UpdateUser(User{Name: "test3", DisplayName: "test3display", Admin: false})
+		user, err := UpdateUser(User{Name: "test3", DisplayName: "test3after", Admin: false})
 		assert.NoError(err)
 		assert.NotEmpty(user)
-		assert.Equal("test3display", user.DisplayName)
+		assert.Equal("test3after", user.DisplayName)
+		assert.Equal(false, user.Admin)
 
 		user2, err := GetUserByName("test3")
 		assert.NoError(err)
 		assert.NotEmpty(user)
-		assert.Equal(user2.DisplayName, user1.DisplayName)
+		assert.NotEqual(user2.DisplayName, user1.DisplayName)
 		assert.NotEqual(user2.Admin, user1.Admin)
 	})
 }
