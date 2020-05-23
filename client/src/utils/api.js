6bbf72ee1f27d8447c4eb9d072726d13a3a0d932
ryoha000 <48485650+ryoha000@users.noreply.github.com>
2020-05-24 05:42:29 +0900

    docker

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,9 +1,5 @@
 import axios from 'axios'
-import { randomString, pkce } from './hash'
 
-// axios.defaults.withCredentials = true
-export const traQBaseURL = 'https://q.trap.jp/api/1.0'
-export const traQClientID = process.env.VUE_APP_API_CLIENT_ID
 axios.defaults.baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : process.env.VUE_APP_API_ENDPOINT
 
 export function getMe () {
